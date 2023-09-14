4572580e46b944787d513911e3067428c3800f33
taku0629 <takkuru2929@gmail.com>
2023-09-14 12:13:21 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+
+  def new
+    # 新しいメッセージの作成フォームを表示する処理
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = Message.new(message_params)
+
+    if @message.save
+      # メッセージが正常に保存された場合の処理
+      redirect_to messages_path(@room)  # メッセージ一覧画面にリダイレクト
+    else
+      # メッセージの保存に失敗した場合の処理
+      render :index, status: :unprocessable_entity
+    end
+  end
+
 end
